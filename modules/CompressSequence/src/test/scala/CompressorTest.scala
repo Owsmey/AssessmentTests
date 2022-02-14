f44edce3b2688e379b30ddce1467543f86cd61a1
Igor Mikryukov <mikrukov.igor@gmail.com>
2022-02-14 17:04:19 +0500

    Changed Property test to Unit Test for better performance

--- modules/CompressSequence/src/test/scala/CompressorTest.scala
+++ modules/CompressSequence/src/test/scala/CompressorTest.scala
@@ -1,36 +1,23 @@
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 import CompressSequence.Process.EmptyInstances._
 import CompressSequence.Process.Compressor._
 
-class CompressorTest extends AnyFlatSpec with ScalaCheckPropertyChecks {
+class CompressorTest extends AnyFlatSpec {
   "Compress" should "remove any number of leading zero-element" in {
-    forAll {(n: Int) =>
-      whenever(n > 0) {
-        val input = Iterator.fill(n)(emptyString.emptyValue)
-        assert(compress(input ++ Iterator.single("A")).toList === List("A"))
-      }
-    }
+    val input = Iterator.fill(Int.MaxValue)(emptyString.emptyValue)
+    assert(compress(input ++ Iterator.single("A")).toList === List("A"))
   }
   it should "remove any number of trailing zero-element" in {
-    forAll { (n: Int) =>
-      whenever(n > 0) {
-        val input = Iterator.fill(n)(emptyString.emptyValue)
-        assert(compress(Iterator.single("A") ++ input).toList === List("A"))
-      }
-    }
+    val input = Iterator.fill(Int.MaxValue)(emptyString.emptyValue)
+    assert(compress(Iterator.single("A") ++ input).toList === List("A"))
   }
   it should "reduce any number of zero-element in-between to one" in {
-    forAll { (n: Int) =>
-      whenever(n > 0) {
-        val input1 = Iterator.fill(n)(emptyString.emptyValue)
-        val input2 = Iterator.fill(n)(emptyString.emptyValue)
-        assert(
-          compress(Iterator.single("A") ++ input1 ++ Iterator.single("A") ++ input2 ++ Iterator.single("A")).toList ===
-            List("A", emptyString.emptyValue, "A", emptyString.emptyValue, "A")
-        )
-      }
-    }
+    val input1 = Iterator.fill(Int.MaxValue)(emptyString.emptyValue)
+    val input2 = Iterator.fill(Int.MaxValue)(emptyString.emptyValue)
+    assert(
+      compress(Iterator.single("A") ++ input1 ++ Iterator.single("A") ++ input2 ++ Iterator.single("A")).toList ===
+        List("A", emptyString.emptyValue, "A", emptyString.emptyValue, "A")
+    )
   }
   it should "works properly on example :)" in {
     val input = List("", "abc", "123", "", "x", "", "", "y", "", "").iterator
